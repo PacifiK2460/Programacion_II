53491f003e92054c0a983a78019fb85d0ee2dd78
Santiago de la cruz Martinez Lara <42554588+PacifiK2460@users.noreply.github.com>
2022-08-10 10:21:05 -0500

    todays work

---
+++ Sesiones/libs/project/trabajador.h
@@ -0,0 +1,15 @@
+#ifndef TRABAJADOR_H
+#define TRABAJADOR_H
+
+typedef struct date{
+    int day;
+    int month;
+    int year;
+} date;
+
+typedef struct Trabajador{
+    int id;
+    
+} Trabajador
+
+#endif
