dfba05162911cc00fde2c125cec10a9bb483c347
PacifiK2460 <chago1929@hotmail.com>
2022-08-15 19:16:12 -0500

    minor changes

--- libs/project/src/zona.c
+++ libs/project/src/zona.c
@@ -47,7 +47,7 @@ int loadZonas(LList* Destination){
     }
     
     // we create a new node with the name and the cost
-    Zona* new = (Zona*)calloc(sizeof(Zona));
+    Zona* new = (Zona*)calloc(1,sizeof(Zona));
     if(new == 0){
       fclose(archivo);
       return -1;
