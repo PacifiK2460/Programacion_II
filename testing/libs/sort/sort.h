219866ebc4954e92e13fd8142cdee3e2aa18a065
Santiago de la cruz Martinez Lara <chago1929@hotmail.com>
2022-09-12 10:30:16 -0500

    min changes

---
+++ testing/libs/sort/sort.h
@@ -0,0 +1,7 @@
+#ifndef SORT_H
+#define SORT_H
+
+// Merge Sort: Array start and end index
+void mergesort(void* arr, void* size, int(*compar)(const void *, const void *));
+
+#endif
--- testing/sort/sort.h
+++
@@ -1,7 +0,0 @@
-#ifndef SORT_H
-#define SORT_H
-
-// Merge Sort: Array start and end index
-void mergesort(void* arr, void* size, int(*compar)(const void *, const void *));
-
-#endif
