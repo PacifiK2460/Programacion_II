7bf9cc8979c4bb3dfb57987e5dc514bb24188637
Santiago de la cruz Martinez Lara <chago1929@hotmail.com>
2022-09-13 10:21:21 -0500

    OU1

---
+++ Sesiones/OmegaUP/Llamadas_telefonicas.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    int ZONA = 0, CANTIDAD = 0;
+    scanf("%d,%d\n", &ZONA, &CANTIDAD);
+    switch (ZONA){
+    case 12:
+        CANTIDAD *= 2;
+        break;
+    case 15:
+        CANTIDAD *= 2.2;
+        break;
+    case 18:
+        CANTIDAD *= 4.5;
+        break;
+    case 19:
+        CANTIDAD *= 3.5;
+        break;
+    case 23:
+        CANTIDAD *= 6;
+        break;
+    case 25:
+        CANTIDAD *= 6;
+        break;
+    case 29:
+        CANTIDAD *= 5;
+        break;
+    }
+    printf("%d", CANTIDAD);
+
+    return 0;
+}
