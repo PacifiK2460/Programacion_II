993d70858debb30df008b598d13f4d6c9e0c0119
Santiago de la cruz Martinez Lara <chago1929@hotmail.com>
2022-09-13 10:44:22 -0500

    yes

--- Sesiones/OmegaUP/Sana_o_insana_distancia.c
+++ Sesiones/OmegaUP/Sana_o_insana_distancia.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
     long X1, Y1, X2, Y2;
     scanf("%ld %ld %ld %ld", &X1, &Y1, &X2, &Y2);
-    long distancia = (X2-X1)*(X2-X1) + (Y2-Y1)*(Y2-Y1);
-    if(distancia <= 150*150){
+
+    // Distancia entre dos puntos
+    long distancia = sqrt((X2-X1)*(X2-X1) + (Y2-Y1)*(Y2-Y1));
+    if(distancia <= 150){
         printf("Sana");
     }else{
         printf("Insana");
