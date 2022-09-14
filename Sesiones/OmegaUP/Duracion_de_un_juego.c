748ad1d77b04a628544fd24b53c530d830b32c6a
PacifiK2460 <chago1929@hotmail.com>
2022-09-13 22:36:48 -0500

    OmegaUp

--- Sesiones/OmegaUP/Duracion_de_un_juego.c
+++ Sesiones/OmegaUP/Duracion_de_un_juego.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int S,F;
-    scanf("%d %d",&S,&F);
+    // Given start and end time print the duration of the game
+    int startHour, endHour;
+    scanf("%d %d", &startHour, &endHour);
+    if(startHour > endHour)
+        printf("%d", (24 - startHour) + endHour);
+    else if(startHour < endHour)
+        printf("%d", endHour - startHour);
+    else if(startHour == endHour)
+        printf("%d", 24);
+
 }
--- Sesiones/OmegaUP/Llamadas_telefonicas.c
+++ Sesiones/OmegaUP/Llamadas_telefonicas.c
@@ -3,31 +3,50 @@
 int main(){
     int ZONA = 0;
     double CANTIDAD = 0;
-    scanf("%d,%lf", &ZONA, &CANTIDAD);
-    switch (ZONA){
-    case 12:
-        CANTIDAD *= 2;
-        break;
-    case 15:
-        CANTIDAD *= 2.2;
-        break;
-    case 18:
-        CANTIDAD *= 4.5;
-        break;
-    case 19:
-        CANTIDAD *= 3.5;
-        break;
-    case 23:
-        CANTIDAD *= 6;
-        break;
-    case 25:
-        CANTIDAD *= 6;
-        break;
-    case 29:
-        CANTIDAD *= 5;
-        break;
+    while (1)
+    {
+        if(ZONA != 0)
+            printf("\n");
+
+        if(scanf("%d %lf", &ZONA, &CANTIDAD) == EOF)
+            break;
+
+        if(CANTIDAD < 0)
+            CANTIDAD *= -1;
+
+        switch (ZONA){
+        case 12:
+            CANTIDAD *= 2;
+            break;
+        case 15:
+            CANTIDAD *= 2.2;
+            break;
+        case 18:
+            CANTIDAD *= 4.5;
+            break;
+        case 19:
+            CANTIDAD *= 3.5;
+            break;
+        case 23:
+            CANTIDAD *= 6;
+            break;
+        case 25:
+            CANTIDAD *= 6;
+            break;
+        case 29:
+            CANTIDAD *= 5;
+            break;
+        default:
+            CANTIDAD *= 0;
+            break;
+        }
+        if(CANTIDAD == (int)CANTIDAD)
+            printf("%d", (int)CANTIDAD);
+        else
+            printf("%.1lf", CANTIDAD);
+
     }
-    printf("%lf", CANTIDAD);
+    
 
     return 0;
 }
