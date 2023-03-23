6a76ed212477d334501e2ce0950e9d4dcd953e39
Santiago de la cruz Martinez Lara <177685@upslp.edu.mx>
2023-03-22 19:53:52 -0600

    Hoy

--- Clase/13.03.2023/main.c
+++ Clase/13.03.2023/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 typedef struct node{
     int data;
@@ -116,16 +117,74 @@ void buscarUnElemento(CircularLinkedList *list){
     printf("El numero %d no se encuentra en la lista\n", num);
 }
 
+void insertarOrdenadamente(CircularLinkedList *list){
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    printf("Ingrese un numero: ");
+    scanf(" %d", &newNode->data);
+    newNode->next = NULL;
+
+    if(list->head == NULL){
+        list->head = newNode;
+        list->tail = newNode;
+        newNode->next = newNode;
+    }else{
+        Node *temp = list->head;
+        if(newNode->data < temp->data){
+            newNode->next = list->head;
+            list->head = newNode;
+            list->tail->next = newNode;
+        }else{
+            while(temp->next != list->head && temp->next->data < newNode->data){
+                temp = temp->next;
+            }
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+    }
+}
+
+void eliminarUnElemento(CircularLinkedList *list){
+    if(list->head == NULL){
+        printf("Lista vacia\n");
+        return;
+    }
+
+    int num = 0;
+    printf("Ingrese un numero: ");
+    scanf(" %d", &num);
+
+    Node *temp = list->head;
+    if(temp->data == num){
+        list->head = list->head->next;
+        list->tail->next = list->head;
+        free(temp);
+    }else{
+        while(temp->next != list->head && temp->next->data != num){
+            temp = temp->next;
+        }
+        if(temp->next == list->head){
+            printf("El numero %d no se encuentra en la lista\n", num);
+        }else{
+            Node *temp2 = temp->next;
+            temp->next = temp->next->next;
+            free(temp2);
+        }
+    }
+}
+
 int main(){
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    
     int opcion = 0;
     CircularLinkedList *list = createCircularLinkedList();
     while(1){
         printf("1. Insertar al inicio\n");
         printf("2. Insertar al final\n");
-        //printf("3. Insertar ordenadamente\n");
+        printf("3. Insertar ordenadamente\n");  
         printf("4. Eliminar al inicio\n");
         printf("5. Eliminar al final\n");
-        //printf("6. Eliminar un elemento\n");
+        printf("6. Eliminar un elemento\n");
         printf("7. Buscar un elemento\n");
         printf("8. Imprimir lista\n");
         printf("0. Salir\n");
@@ -141,7 +200,7 @@ int main(){
                 insertarAlFinal(list);
                 break;
             case 3:
-                //insertarOrdenadamente(list);
+                insertarOrdenadamente(list);
                 break;
             case 4:
                 eliminarAlInicio(list);
@@ -150,7 +209,7 @@ int main(){
                 eliminarAlFinal(list);
                 break;
             case 6:
-                //eliminarUnElemento(list);
+                eliminarUnElemento(list);
                 break;
             case 7:
                 buscarUnElemento(list);
